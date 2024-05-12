ae36d15986417277f16dea35f7dfe6832c5f2cb6
ghun814 <hung080104@gmail.com>
2024-05-12 17:46:41 +0700

    Create table
    Create table Category, Bakery, BakeryID

--- Database/KingBakeryManagement.sql
+++ Database/KingBakeryManagement.sql
@@ -9,9 +9,9 @@ CREATE TABLE Users (
 	FullName NVARCHAR(100) NOT NULL,
 	Username NVARCHAR(100) NOT NULL,
 	Password NVARCHAR(100) NOT NULL,
-	Address NVARCHAR(200) NOT NULL,
+	Address NVARCHAR(200),
 	BirthDate DateTime NOT NULL,
-	Email NVARCHAR(100) NOT NULL,
+	Email NVARCHAR(100),
     PhoneNumber NVARCHAR(100) NOT NULL,
 	Role INT NOT NULL
 )
@@ -22,7 +22,6 @@ CREATE TABLE Employee (
 	Salary FLOAT NOT NULL,
 	HiredDate DateTime NOT NULL,
 	Status NVARCHAR(100) NOT NULL,
-
 	FOREIGN KEY (UserID) REFERENCES Users(ID) on delete cascade
 )
 GO
@@ -32,5 +31,30 @@ CREATE TABLE Customer (
 	Ranking NVARCHAR(10) NOT NULL,
 
 	FOREIGN KEY (UserID) REFERENCES Users(ID) on delete cascade
+)
+GO
+
+CREATE TABLE Category(
+	ID INT Identity(1,1) PRIMARY KEY,
+	Name NVARCHAR(255)
+)
+GO
+
+CREATE TABLE Bakery(
+	ID INT Identity(1,1) PRIMARY KEY,
+	Size INT,
+	Quantity INT,
+	Price FLOAT
+)
+GO
+
+CREATE TABLE BakeryDetail(
+	BakeryID INT PRIMARY KEY,
+	Name NVARCHAR(255),
+	Image VARCHAR(255),
+	Description NVARCHAR(4000),
+	CategoryID INT, 
+	FOREIGN KEY (BakeryID) REFERENCES Bakery(BakeryID) on delete cascade,
+	FOREIGN KEY (CategoryID) REFERENCES Category(CategoryID) on delete cascade
 )
 GO
