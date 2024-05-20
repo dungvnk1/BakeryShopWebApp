1aa78fe489000620135ba8549c469388c36ab007
vnd_vunangdung <126397851+dungvnk1@users.noreply.github.com>
2024-05-20 23:27:50 +0700

    Create project in Visual Studio

---
+++ Project/KingBakery/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KingBakery.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
