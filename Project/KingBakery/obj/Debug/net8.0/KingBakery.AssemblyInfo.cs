2b0a0d19a979566525b3c9ba1ae27852aee0a538
vnd_vunangdung <126397851+dungvnk1@users.noreply.github.com>
2024-05-30 20:26:44 +0700

    Fix UI

--- Project/KingBakery/obj/Debug/net8.0/KingBakery.AssemblyInfo.cs
+++ Project/KingBakery/obj/Debug/net8.0/KingBakery.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KingBakery")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+606a45d097d1f165c850b8d0981748af226c8f70")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eced5b5a4ec4e10f134c6aae95337b34db86a36f")]
 [assembly: System.Reflection.AssemblyProductAttribute("KingBakery")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KingBakery")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
