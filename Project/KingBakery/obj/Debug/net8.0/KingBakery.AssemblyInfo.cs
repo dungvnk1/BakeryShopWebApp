f23affdbf05a6a410fafb4563a6704ef38f28a5f
ghun814 <hung080104@gmail.com>
2024-05-30 21:31:55 +0700

    Update cart

--- Project/KingBakery/obj/Debug/net8.0/KingBakery.AssemblyInfo.cs
+++ Project/KingBakery/obj/Debug/net8.0/KingBakery.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KingBakery")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+606a45d097d1f165c850b8d0981748af226c8f70")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d0f351df18e2475ca25f02704d880e0bbfd9547")]
 [assembly: System.Reflection.AssemblyProductAttribute("KingBakery")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KingBakery")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
