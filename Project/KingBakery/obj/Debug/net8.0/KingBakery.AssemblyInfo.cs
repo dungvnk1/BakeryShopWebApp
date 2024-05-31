966102cf13c55b18805aef8e5ba97ef9ad03ece8
vnd_vunangdung <126397851+dungvnk1@users.noreply.github.com>
2024-05-31 15:36:16 +0700

    Fix bug merge

--- Project/KingBakery/obj/Debug/net8.0/KingBakery.AssemblyInfo.cs
+++ Project/KingBakery/obj/Debug/net8.0/KingBakery.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KingBakery")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+595605d5b80a6fdbad30fa8af38dba5147d68a9f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("KingBakery")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KingBakery")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
