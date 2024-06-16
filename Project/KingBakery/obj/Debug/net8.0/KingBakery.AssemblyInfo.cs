f7bf87069e2024c01a113725bf247712e1d8eaf4
phucch11 <153418387+LongSuy@users.noreply.github.com>
2024-06-16 14:23:30 +0700

    not important

--- Project/KingBakery/obj/Debug/net8.0/KingBakery.AssemblyInfo.cs
+++ Project/KingBakery/obj/Debug/net8.0/KingBakery.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KingBakery")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d5f031cc801cfd2a9cfa1aaba110035c2727048")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c033533e09294e26211e566a4820662ed2f62a98")]
 [assembly: System.Reflection.AssemblyProductAttribute("KingBakery")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KingBakery")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
